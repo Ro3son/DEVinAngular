ba2651d4d16d20e3fa7474a9a9ca63e4ed1270c8
Robson De Andrade <theairelibre@protonmail.com>
2022-04-22 16:15:55 -0300

    formGropu FormControl

--- my-app/src/app/login/login.component.ts
+++ my-app/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,21 +9,17 @@ import { Validators, FormBuilder } from '@angular/forms';
   // Component class //
 export class LoginComponent implements OnInit {
   // Properties / variables //
-
   title: string = 'Log In'
 
-  constructor(private fb: FormBuilder) {}
-
-  form = this.fb.group({
-    email: ['', Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[az]{2,4}$")],  // regex string and numbers
-    password: ['', Validators.required, Validators.pattern("^\d{10}$")] // regex 10 numbers only
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
   })
-  
   // Methods //
   onSubmit() {
     console.warn(this.form.value);
   }
-  // Lifecycle Hook
+  // Lifecycle Hook //
   ngOnInit(): void {
   }
 }
