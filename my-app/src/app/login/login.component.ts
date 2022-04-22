8c53d34453a18bc95c90ef3c39a4444c9386ffb5
Robson De Andrade <theairelibre@protonmail.com>
2022-04-22 11:49:09 -0300

    Validating form input

--- my-app/src/app/login/login.component.ts
+++ my-app/src/app/login/login.component.ts
@@ -1,26 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
-import { FormGroup, FormControl } from '@angular/forms';
+import { Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-// Component class //
+  // Component class //
 export class LoginComponent implements OnInit {
-  // Create properties //
+  // Properties / variables //
+
   title: string = 'Log In'
 
-  form = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+  constructor(private fb: FormBuilder) {}
+
+  form = this.fb.group({
+    email: ['', Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[az]{2,4}$")],  // regex string and numbers
+    password: ['', Validators.required, Validators.pattern("^\d{10}$")] // regex 10 numbers only
   })
   
+  // Methods //
   onSubmit() {
     console.warn(this.form.value);
   }
-  
+  // Lifecycle Hook
   ngOnInit(): void {
   }
 }
