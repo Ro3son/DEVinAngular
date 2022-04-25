c24e9729d7c079b16dd0f0208d394b506554a78f
Robson De Andrade <theairelibre@protonmail.com>
2022-04-25 15:37:16 -0300

    create variable input

--- my-app/src/app/components/search/search.component.ts
+++ my-app/src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-
+  input = new FormControl('');
   constructor() { }
 
   ngOnInit(): void {
