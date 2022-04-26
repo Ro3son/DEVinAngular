daf4729a4f1e77dba05ad1f4234b3c508a849050
Robson De Andrade <theairelibre@protonmail.com>
2022-04-26 11:45:03 -0300

    create methods for basic math operators

--- my-app/src/app/components/number/number.component.ts
+++ my-app/src/app/components/number/number.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NumberComponent implements OnInit {
 
+  // Variables / Properties //
+  public inputValue1: number = 0;
+
+  public inputValue2: number = 0;
+
+  public resultado: any;
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
+  // Functions for all of the basic math operators //
+  add() {
+    const soma = this.inputValue1 + this.inputValue2;
+    this.resultado = soma;
+  }
+  subtract() {
+    const subtrai = this.inputValue1 - this.inputValue2;
+    console.log(subtrai);
+  }
+  multiply() {
+    const multiplica = this.inputValue1 * this.inputValue2;
+    console.log(multiplica);
+  }
+  divide() {
+    const divide = this.inputValue1 / this.inputValue2;
+    console.log(divide);
+  }
+  clear() {
+    this.resultado = '';
+    this.inputValue1 = 0;
+    this.inputValue2 = 0;
+  }
 }
