9001cbf1f6a62bc2e18452c4bbc5adab465ea2dc
Robson De Andrade <theairelibre@protonmail.com>
2022-04-28 16:13:59 -0300

    import component route c

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './components/login/login.component';
 import { SearchComponent } from './components/search/search.component';
 import { NumberComponent } from './components/number/number.component';
 import { FormComponent } from './components/form/form.component';
+import { FilmesAPIComponent } from './components/filmes-api/filmes-api.component';
 
 const routes: Routes = [
   {
@@ -17,7 +18,8 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'search', component: SearchComponent },
   { path: 'number', component: NumberComponent },
-  { path: 'form', component: FormComponent }
+  { path: 'form', component: FormComponent },
+  { path: 'filmesAPI', component: FilmesAPIComponent}
 ];
 
 @NgModule({
