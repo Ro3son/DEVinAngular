d1360f82afddeabb369445595c14c6772db4e726
Robson De Andrade <theairelibre@protonmail.com>
2022-04-28 16:15:35 -0300

    import HttpClient

--- my-app/src/app/services/service-test.service.ts
+++ my-app/src/app/services/service-test.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceTestService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  realizaGet() {
+    const requestURL = environment.filmesAPI;
+    console.log(requestURL);
+    this.http.get(requestURL);
+  }
 }
