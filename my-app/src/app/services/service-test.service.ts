71663b436cb5fd4f2977f6867f180ca169e1d7cd
Robson De Andrade <theairelibre@protonmail.com>
2022-04-29 15:33:45 -0300

    request Url API ng

--- my-app/src/app/services/service-test.service.ts
+++ my-app/src/app/services/service-test.service.ts
@@ -8,9 +8,24 @@ import { environment } from 'src/environments/environment';
 export class ServiceTestService {
 
   constructor(private http: HttpClient) { }
-  realizaGet() {
-    const requestURL = environment.filmesAPI;
-    console.log(requestURL);
-    this.http.get(requestURL);
+
+  // Headers = {
+  //   Authorization: 'beared fdfd'
+  // }
+
+  // public requestUrl = 'https://api.themoviedb.org/3/movie/110d2cce13d7bd6d5cf7c9157bf67ac8';
+
+public getFilmes(upcoming: any) {
+     new Promise((resolve, reject) => {
+      this.http.get(environment.filmesAPI + upcoming + '?api_key=110d2cce13d7bd6d5cf7c9157bf67ac8')
+        .toPromise().then((data: any) => {
+          console.log(data.results[0].original_title);
+          resolve(data);
+        })
+        .catch((error) => {
+          console.log(error);
+          reject(error);
+        });
+    });
   }
 }
