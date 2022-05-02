87f5ef856c89a90eee5525fce812870f5159bc4d
Robson De Andrade <theairelibre@protonmail.com>
2022-05-02 14:20:52 -0300

    add variable p1

--- app-jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.component.ts
+++ app-jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -6,10 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./jogo-da-velha.component.scss']
 })
 export class JogoDaVelhaComponent {
-
+// Variables //
   public title: string = '< jogo-da-véia />';
 
   public player1: string = '';
+
+  public p1: string = '';
 
   public player2: string = '';
 
@@ -22,28 +24,42 @@ export class JogoDaVelhaComponent {
     ['', '', '']
   ]
   constructor() { }
-
+// Functions //
   public select1() {
-    this.player1 = 'O', 
-    this.player2 = 'X';
+    this.player1 = 'O',
+      this.player2 = 'X';
   }
   public select2() {
-    this.player1 = 'X', 
-    this.player2 = 'O';
+    this.player1 = 'X',
+      this.player2 = 'O';
   }
   public playRound(line: number, col: number) {
-    if (this.board[line][col] == '') {
-      this.board[line][col] = this.player1;
+    if (this.board[line][col] == '' && this.winner == '') {
+      this.board[line][col] = this.p1
+      if (this.checkWinner(this.p1)) {
+        this.winner = this.p1;
+      }
     }
-    if (this.player1 == 'O') {
-        this.player1;
-    } 
-    if (this.player2 == 'X') {
-      this.player2;
-    } 
+    let changePlayer = (this.p1 == 'O') ? this.p1 = 'X' : this.p1 = 'O';
   }
-  public checkWinner(player: string) {
-
+  public checkWinner(player: string): boolean {
+    for (let i = 0; i < this.board.length; i++) {
+      if (this.board[i][0] == player && this.board[i][1] == player && this.board[i][2] == player) {
+        return true;
+      }
+    }
+    for (let i = 0; i < this.board.length; i++) {
+      if (this.board[0][i] == player && this.board[1][i] == player && this.board[2][i] == player) {
+        return true;
+      }
+    }
+    if (this.board[0][0] == player && this.board[1][1] == player && this.board[2][2]) {
+      return true;
+    }
+    if (this.board[0][2] == player && this.board[1][1] == player && this.board[2][0]) {
+      return true;
+    }
+    return false;
   }
   public reset() {
     this.player1 = '';
