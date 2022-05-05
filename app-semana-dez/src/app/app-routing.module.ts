9b179a7f998b6f67f0fb7b82058e35bec527d139
Robson De Andrade <theairelibre@protonmail.com>
2022-05-05 15:20:03 -0300

    import new components

--- app-semana-dez/src/app/app-routing.module.ts
+++ app-semana-dez/src/app/app-routing.module.ts
@@ -3,12 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { FullComponent } from './layouts/full/full.component';
+import { ContentComponent } from './layouts/content/content.component';
+import { SigninComponent } from './layouts/signin/signin.component';
+import { PasswordComponent } from './layouts/password/password.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
-  { path: 'full', component: FullComponent},
-  
+
+  // Declara FullComponent como pai de todas as rotas //
+  // { path: '', redirectTo: 'full', pathMatch: 'full'},
+  // { path: 'full', component: FullComponent},
+  // { path: '', component: ContentComponent,
+  // children: [
+  //     { path: 'signin', component: SigninComponent},
+  //     { path: 'password', component: PasswordComponent},
+  //   ]
+  // },
 ];
 
 @NgModule({
