4ea61a77970a566ef8c8fac7c91aa2135b22cb08
Robson De Andrade <theairelibre@protonmail.com>
2022-05-06 11:27:02 -0300

    route path c informarEmail

--- app-semana-dez/src/app/app-routing.module.ts
+++ app-semana-dez/src/app/app-routing.module.ts
@@ -2,24 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { FullComponent } from './layouts/full/full.component';
-import { ContentComponent } from './layouts/content/content.component';
-import { SigninComponent } from './layouts/signin/signin.component';
-import { PasswordComponent } from './layouts/password/password.component';
+import { InformarEmailComponent } from './informar-email/informar-email.component';
 
 const routes: Routes = [
+
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
+  { path: 'email', component: InformarEmailComponent}
 
-  // Declara FullComponent como pai de todas as rotas //
-  // { path: '', redirectTo: 'full', pathMatch: 'full'},
-  // { path: 'full', component: FullComponent},
-  // { path: '', component: ContentComponent,
-  // children: [
-  //     { path: 'signin', component: SigninComponent},
-  //     { path: 'password', component: PasswordComponent},
-  //   ]
-  // },
 ];
 
 @NgModule({
