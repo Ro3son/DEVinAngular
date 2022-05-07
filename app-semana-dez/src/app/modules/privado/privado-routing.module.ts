37a91db66be8a0efb284e61c331dff0182636e47
Robson De Andrade <theairelibre@protonmail.com>
2022-05-07 15:58:17 -0300

    remove path content not found

--- app-semana-dez/src/app/modules/privado/privado-routing.module.ts
+++ app-semana-dez/src/app/modules/privado/privado-routing.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from '../../home/home.component';
-import { ContentComponent } from 'src/app/layouts/content/content.component';
-import { NotFoundComponent } from 'src/app/not-found/not-found.component';
 
 const routes: Routes = [
   
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
-  { path: 'content', component: ContentComponent},
-  { path: '**', component: NotFoundComponent}
 
 ];
 
