ee84900b23b11a7955d61418fe90aad9f6a2776d
Robson De Andrade <theairelibre@protonmail.com>
2022-05-07 12:04:41 -0300

    router modules

--- app-semana-dez/src/app/app-routing.module.ts
+++ app-semana-dez/src/app/app-routing.module.ts
@@ -1,30 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { InformarEmailComponent } from './informar-email/informar-email.component';
+// import { HomeComponent } from './home/home.component';
+// import { InformarEmailComponent } from './informar-email/informar-email.component';
 
 const routes: Routes = [
 
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent},
-  { path: 'email', component: InformarEmailComponent}
+  // { path: '', redirectTo: 'home', pathMatch: 'full'},
+  // { path: 'home', component: HomeComponent},
+  // { path: 'email', component: InformarEmailComponent}
 
-
-  // {
-  //   path: 'home',
-  //   loadChildren: () => import('./modules/privado/privado.module').then(m => m.PrivadoModule)
-  // },
-  // {
-  //   path: 'login',
-  //   loadChildren: () => import('./modules/publico/publico.module').then(m => m.PublicoModule)
-  // },
-  // {
-  //   path: '',
-  //   redirectTo: '',
-  //   pathMatch: 'full'
-  // }
-
+  //  Roteamento entre módulos //
+  {
+    path: 'privado',
+    loadChildren: () => import('./modules/privado/privado.module').then(m => m.PrivadoModule)
+  },
+  {
+    path: 'publico',
+    loadChildren: () => import('./modules/publico/publico.module').then(m => m.PublicoModule)
+  }
 ];
 
 @NgModule({
