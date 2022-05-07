c81570cace6b72aa497ce95195d467cc80960f3c
Robson De Andrade <theairelibre@protonmail.com>
2022-05-07 15:58:42 -0300

    import components private module

--- app-semana-dez/src/app/modules/privado/privado.module.ts
+++ app-semana-dez/src/app/modules/privado/privado.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PrivadoRoutingModule } from './privado-routing.module';
-
+import { SerieComponent } from './serie/serie.component';
+import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SerieComponent,
+    TelaInicialComponent,
+  ],
   imports: [
     CommonModule,
     PrivadoRoutingModule
