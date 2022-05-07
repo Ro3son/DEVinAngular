ad765d684df8a8d38455b5daa9b3ffea370acaf2
Robson De Andrade <theairelibre@protonmail.com>
2022-05-07 12:07:13 -0300

    import c routes path

--- app-semana-dez/src/app/modules/publico/publico-routing.module.ts
+++ app-semana-dez/src/app/modules/publico/publico-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { InformarEmailComponent } from 'src/app/informar-email/informar-email.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'email', pathMatch: 'full'},
+  { path: 'email', component: InformarEmailComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
