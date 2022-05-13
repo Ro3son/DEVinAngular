2ab6f22433e854bfc6ee0bf8cb936dcf2cef0d17
Robson De Andrade <theairelibre@protonmail.com>
2022-05-13 17:57:45 -0300

    observable

--- app-card-star-wars/src/app/services/starwars.service.ts
+++ app-card-star-wars/src/app/services/starwars.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StarwarsService {
 
-  constructor() { }
+  public requestURL = environment.API_Path;
+
+  constructor(private http: HttpClient) { }
+
+  public getPersonagens(): Observable<any[]> {
+    return this.http.get<any[]>(this.requestURL);
+  }
+
+  // public getPersonagens() {
+  //   this.http.get(this.requestURL)
+  //   .toPromise().then((response) => {
+  //    console.log(response);
+  //    return response;
+  //   })
+  //   .catch((error) => {
+  //     console.log('Deu erro: ' + error);
+  //     return error;
+  //   });
+  // }
 }
