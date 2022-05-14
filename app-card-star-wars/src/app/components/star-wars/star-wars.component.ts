385c3f02225bb494a09cd41fba69e9bc4d75bec0
Robson De Andrade <theairelibre@protonmail.com>
2022-05-14 15:57:16 -0300

    method subscribe

--- app-card-star-wars/src/app/components/star-wars/star-wars.component.ts
+++ app-card-star-wars/src/app/components/star-wars/star-wars.component.ts
@@ -1,31 +1,27 @@
 import { Component } from '@angular/core';
 import { Starwars } from 'src/app/classes/starwars';
 import { StarwarsService } from 'src/app/services/starwars.service';
-import { OnInit } from '@angular/core';
-import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-star-wars',
   templateUrl: './star-wars.component.html',
   styleUrls: ['./star-wars.component.scss']
 })
-export class StarWarsComponent implements OnInit {
+export class StarWarsComponent {
 
-  public personagens!: any[];
+  public personagens: Starwars[] = [];
 
   public error: boolean = false;
 
   constructor(private starwarsService: StarwarsService) {
 
   }
-  ngOnInit() {
-    this.obterTodosPersonagens();
-  }
   // Trazer todos os dados //
   public obterTodosPersonagens() {
-    this.starwarsService.getPersonagens().subscribe((dados: any) => {
+    this.starwarsService.getPersonagens().subscribe((dados) => {
       this.error = false;
-      this.personagens = dados;
+      this.personagens = dados as [];
+      console.log(JSON.stringify(this.personagens));
     },
       (erro: any) => {
         console.warn('Deu erro', erro);
@@ -34,7 +30,15 @@ export class StarWarsComponent implements OnInit {
   }
   // Trazer dados do ID escolhido //
   public obterIdPersonagem() {
-
+    this.starwarsService.obterSomenteUm().subscribe((dados) => {
+      this.error = false;
+      this.personagens = dados as [];
+      console.log(JSON.stringify(this.personagens));
+    },
+      (erro: any) => {
+        console.warn('Deu erro aqui', erro);
+        this.error = true;
+      })
   }
   // Adicionar dados //
   public adicionarPersonagem() {
