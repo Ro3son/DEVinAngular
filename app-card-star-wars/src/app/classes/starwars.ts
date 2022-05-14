e3a981aa567a73d70c5ebace624830952da2add3
Robson De Andrade <theairelibre@protonmail.com>
2022-05-14 15:53:55 -0300

    add property info

--- app-card-star-wars/src/app/classes/starwars.ts
+++ app-card-star-wars/src/app/classes/starwars.ts
@@ -1,17 +1,19 @@
 export class Starwars {
-    id: number;
+    id: number
     nome: string;
     habilidade: string;
     planeta: string;
     armas: string;
     avatar: string;
+    info: string;
 
-    constructor(id: number, nome: string, habilidade: string, planeta: string, armas: string, avatar: string) {
+    constructor(id: number, nome: string, habilidade: string, planeta: string, armas: string, avatar: string, info: string) {
         this.id = id;
         this.nome = nome;
         this.habilidade = habilidade;
         this.planeta = planeta;
         this.armas = armas;
         this.avatar = avatar;
+        this.info = info;
     }
 }
