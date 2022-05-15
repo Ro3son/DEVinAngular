84d9ad88de1146d97f3d6028db8bbceac16ff06c
Robson De Andrade <theairelibre@protonmail.com>
2022-05-15 15:49:36 -0300

    import ReactiveFormsModule

--- app-card-star-wars/src/app/app.module.ts
+++ app-card-star-wars/src/app/app.module.ts
@@ -6,16 +6,21 @@ import { AppComponent } from './app.component';
 import { StarWarsComponent } from './components/star-wars/star-wars.component';
 import { HttpClientModule } from '@angular/common/http'
 import { StarwarsService } from './services/starwars.service';
+import { KeyobjectPipe } from './pipes/keyobject.pipe';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StarWarsComponent
+    StarWarsComponent,
+    KeyobjectPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [
     StarwarsService
