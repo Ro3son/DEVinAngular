c3a7c87b30cdccb04357edf10316baf24d392d74
Robson De Andrade <theairelibre@protonmail.com>
2022-05-15 21:52:30 -0300

    put() method

--- app-card-star-wars/src/app/services/starwars.service.ts
+++ app-card-star-wars/src/app/services/starwars.service.ts
@@ -19,15 +19,15 @@ export class StarwarsService {
   }
   // Requisita somente uma informação //
   public obterSomenteUm(): Observable<Starwars[]> {
-    return this.http.get<Starwars[]>(`${this.requestURL}/${2}`);
+    return this.http.get<Starwars[]>(`${this.requestURL}/${5}`);
   }
   // Cadastra informações //
   public postPersonagem(data: any): Observable<Starwars[]> {
     return this.http.post<Starwars[]>(`${this.requestURL}`, data);
   }
   // Atualiza informações //
-  public putPersonagem() {
-
+  public putPersonagem(data: any): Observable<Starwars[]> {
+    return this.http.put<Starwars[]>(`${this.requestURL}/${6}`, data);
   }
   // Deleta informações //
   public delete(id: number): Observable<Starwars[]> {
