22039f87b94a20cbf3e36e1dd048523a70f02ab9
Robson De Andrade <theairelibre@protonmail.com>
2022-05-31 11:31:43 -0300

    add c in declarations

--- app-card-star-wars/src/app/app.module.ts
+++ app-card-star-wars/src/app/app.module.ts
@@ -8,12 +8,16 @@ import { HttpClientModule } from '@angular/common/http'
 import { StarwarsService } from './services/starwars.service';
 import { KeyobjectPipe } from './pipes/keyobject.pipe';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EditarPersonagemComponent } from './components/editar-personagem/editar-personagem.component';
+import { CadastrarPersonagemComponent } from './components/cadastrar-personagem/cadastrar-personagem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StarWarsComponent,
-    KeyobjectPipe
+    KeyobjectPipe,
+    EditarPersonagemComponent,
+    CadastrarPersonagemComponent
   ],
   imports: [
     BrowserModule,
