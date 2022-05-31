9b0907f3ecf86acfd6105d543d821845845c11a3
Robson De Andrade <theairelibre@protonmail.com>
2022-05-31 11:29:11 -0300

    add routes paths

--- app-card-star-wars/src/app/app-routing.module.ts
+++ app-card-star-wars/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarPersonagemComponent } from './components/cadastrar-personagem/cadastrar-personagem.component';
+import { EditarPersonagemComponent } from './components/editar-personagem/editar-personagem.component';
+import { StarWarsComponent } from './components/star-wars/star-wars.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: StarWarsComponent },
+  { path: 'editarPersonagem', component: EditarPersonagemComponent },
+  { path: 'cadastrarPersonagem', component: CadastrarPersonagemComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
