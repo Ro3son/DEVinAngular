95af563770022fce646f99c6934ed652d188ff0a
Robson De Andrade <theairelibre@protonmail.com>
2022-06-02 15:28:45 -0300

    create new c

---
+++ app-card-star-wars/src/app/components/personagem-id/personagem-id.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personagem-id',
+  templateUrl: './personagem-id.component.html',
+  styleUrls: ['./personagem-id.component.scss']
+})
+export class PersonagemIdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
