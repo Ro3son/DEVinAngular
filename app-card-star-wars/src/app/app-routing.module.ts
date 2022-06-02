3a8909d9ce2ab1a4837c035333f4156c91615c24
Robson De Andrade <theairelibre@protonmail.com>
2022-06-02 15:29:24 -0300

    add path /:id route parameter

--- app-card-star-wars/src/app/app-routing.module.ts
+++ app-card-star-wars/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarPersonagemComponent } from './components/cadastrar-personagem/cadastrar-personagem.component';
 import { EditarPersonagemComponent } from './components/editar-personagem/editar-personagem.component';
+import { PersonagemIdComponent } from './components/personagem-id/personagem-id.component';
 import { StarWarsComponent } from './components/star-wars/star-wars.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'personagens', pathMatch: 'full' },
   { path: 'personagens', component: StarWarsComponent },
+  { path: 'personagens/:id', component: PersonagemIdComponent },
   { path: 'editar', component: EditarPersonagemComponent },
   { path: 'cadastrar', component: CadastrarPersonagemComponent }
 ];
